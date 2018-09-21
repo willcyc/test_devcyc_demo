f6aed31365a4a96ca8a97b8eb450c00b34c1a2ab
willcyc <13839205941@163.com>
2018-09-21 23:22:17 +0800

    登录

---
+++ test_platform/user_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+#处理登录
+def login_action(request):
+    if request.method == "GET":
+        username = request.GET.get("username")
+        password = request.GET.get("password")
+
+        if username == ""  or password == "":
+            return render(request, "index.html",{"error":"用户名或密码不能为空"})
