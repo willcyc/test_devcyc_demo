f1712d39634e5d1ff384cedffeb77314591eccdc
willcyc <13839205941@163.com>
2018-09-26 23:57:27 +0800

    登录

--- test_platform/test_platform/settings.py
+++ test_platform/test_platform/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_app',
+    'user_app.apps.UserAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import auth
 
 # Create your views here.
 def index(request):
@@ -7,9 +8,24 @@ def index(request):
 
 #处理登录
 def login_action(request):
-    if request.method == "GET":
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username","")
+        password = request.POST.get("password","")
 
         if username == ""  or password == "":
             return render(request, "index.html",{"error":"用户名或密码不能为空"})
+
+        else:   #认证admin/后台的用户
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return render(request,"project_manage.html")
+            else:
+                return render(request,"index.html",{"error":"用户名或密码错误！"})
+
+        '''
+        elif username == "admin" and password == "admin123":
+            return render(request, "project_manage.html")
+        else:
+            return render(request, "index.html", {"error": "用户名或密码错误！"})
+        '''
