64cf254228ce56babd0daeafb41dfbc7a17e6024
willcyc <13839205941@163.com>
2018-10-18 22:50:47 +0800

    创建项目表单

--- test_platform/project_app/urls.py
+++ test_platform/project_app/urls.py
@@ -4,4 +4,5 @@ from project_app import views
 urlpatterns = [
     path('project_manage/',views.project_manage),
     path('add_project/',views.add_project),
+    path('edit_project/<int:pid>/',views.edit_project),
 ]
--- test_platform/project_app/views.py
+++ test_platform/project_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from project_app.models import Project
+from project_app.forms import ProjectForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 @login_required
@@ -11,7 +13,43 @@ def project_manage(request):
     project_all = Project.objects.all()
     return render(request, "project_manage.html",{"user":username,"projects":project_all,"type":"list"})
 
+'''
 @login_required
 def add_project(request):
     """添加项目"""
     return render(request, "project_manage.html",{"type":"add"})
+'''
+
+@login_required
+def add_project(request):
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            describe = form.cleaned_data['describe']
+            Project.objects.create(name=name,describe=describe)
+
+            return HttpResponseRedirect('/manage/project_manage/')
+    else:
+        form = ProjectForm()
+
+    return render(request,"project_manage.html",{'form':form,'type':'add'})
+
+@login_required
+def edit_project(request,pid):
+
+    project = Project.objects.get(id=pid)
+    name = project.name
+    describe = project.describe
+
+    if request.method == 'POST':
+        form = ProjectForm(instance=project,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/manage/project_manage/')
+    else:
+        form = ProjectForm()
+
+    #context = {'project':project,'name':name,'describe':describe,'form':form}
+    return render(request, "project_manage.html", {'form': form, 'type': 'edit','project':project})
