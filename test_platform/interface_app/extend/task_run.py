0669bc5364bedb7452f092a3857e4baec188bcfa
willcyc <13839205941@163.com>
2018-12-09 22:15:52 +0800

    运行用例

--- test_platform/interface_app/extend/task_run.py
+++ test_platform/interface_app/extend/task_run.py
@@ -1,33 +1,52 @@
 #运行某任务下的用例cases --单元测试框架 + 数据驱动
+import sys
 import unittest
-from ddt import ddt,data,unpack
+import json
+from ddt import ddt,data,unpack,file_data
 import requests
 import xmlrunner
+from os.path import dirname,abspath
+
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))  #abspath(__file__)：当前文件的绝对路径；dirname上一级文件
+BASE_PATH = BASE_DIR.replace("\\","/")
+#sys.path.append(BASE_PATH)  #将获取到的项目根目录添加到环境变量中
+TASK_PATH = BASE_PATH + "/resource/tasks/"  #任务的目录
+#print("运行测试文件：",BASE_PATH)
 
 @ddt
 class MyTest(unittest.TestCase):
 
 	@unpack
-	@data({'url': 'http://httpbin.org/post', 'method': 'post', 'data':{'key':'value'}},
-		{'url': 'https://api.github.com/events', 'method': 'get', 'data': {}})
-	def test_dicts_extracted_into_kwargs(self, url, method, data):
+	@file_data(TASK_PATH + "cases_data.json")
+
+	def test_demo(self, url, method, type_,header,parameter,assert_):
 		#print("URL:",url)
 		#print("方法:",method)
-		#print("参数：",data)
+		#print("参数：",parameter)
 
-		if method == "post":
-			r = requests.post(url,data=data)
-			#print(r.json())
-			self.assertEqual(2+1,4)
+		if header == "{}":
+			header_dict = {}
+		else:
+			header_dict = json.loads(header.replace("\'","\""))  #将json字符串转化为字典、将单引号转化为双引号
+
+		if parameter == "{}":
+			parameter_dict = {}
+		else:
+			parameter_dict = json.loads(parameter.replace("\'","\""))  #将json字符串转化为字典、将单引号转化为双引号
 
 		if method == "get":
-			r = requests.get(url,params=data)
-			#print(r.text)
-			self.assertEqual(2+2,4)
+			if type_ == "from":
+				r = requests.get(url, headers=header_dict, params=parameter_dict)
+
+		elif method == "post":
+			if type_ == "from":
+				r = requests.post(url, headers=header_dict, data=parameter_dict)
+			elif type_ == "json":
+				r = requests.post(url, headers=header_dict, json=parameter_dict)
 
 #运行测试用例
 def run_cases():
-    with open('D:/git/test_devcyc_demo/test_platform/interface_app/extend/results.xml', 'wb') as output:  # 指定report存放位置为当前目录
+    with open(TASK_PATH + 'results.xml', 'wb') as output:  # 指定report存放位置为当前目录
         unittest.main(
             testRunner=xmlrunner.XMLTestRunner(output=output),
             failfast=False, buffer=False, catchbreak=False)
--- test_platform/interface_app/views/testtask_views.py
+++ test_platform/interface_app/views/testtask_views.py
@@ -1,9 +1,10 @@
-import os
+import os,json
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-from interface_app.models import TestTask
+from interface_app.models import TestTask,TestCase
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from interface_app.extend.task_run import run_cases
+from interface_app.apps import TASK_PATH,RUN_TASK_FILE
 
 # Create your views here.
 
@@ -36,11 +37,36 @@ def add_task(request):
 def run_task(request,tid):
     if request.method == 'GET':
         task_obj = TestTask.objects.get(id=tid)
+        cases_list = task_obj.cases.split(",")
+        cases_list.pop(-1)
+        print(cases_list)
+
+        task_obj.status = 1   #修改任务执行状态
+        task_obj.save()
         #print(task_obj.cases)
-        case_list = task_obj.cases.split(",")
-        case_list.pop(-1)
-        #print(case_list)
-        os.system("python D:/git/test_devcyc_demo/test_platform/interface_app/extend/task_run.py") #运行用例
+
+        all_cases_dict = {}
+        for case_id in cases_list:
+            case_obj = TestCase.objects.get(id=case_id)
+            case_dict = {
+                "url":case_obj.url,
+                "method":case_obj.req_method,
+                "type_":case_obj.req_type,
+                "header":case_obj.req_header,
+                "parameter":case_obj.req_parameter,
+                "assert_":case_obj.resp_assert
+            }
+            all_cases_dict[case_obj.id] = case_dict
+
+        print(all_cases_dict)
+
+        cases_str = json.dumps(all_cases_dict)   #json格式数据转化为字符串
+        cases_data_file = TASK_PATH + "cases_data.json"  #用例文件路径
+        print(cases_data_file)
+        with open(cases_data_file,"w") as f:   #将用例写入到cases_data.json用例文件中
+            f.write(cases_str)
+
+        os.system("python " + RUN_TASK_FILE)   #运行用例
 
         return HttpResponseRedirect("/interface/task_manage/")
 
