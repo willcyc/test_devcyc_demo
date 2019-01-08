009da3e232d5ab9d0dc03c5d2f6df74882d88347
willcyc <13839205941@163.com>
2019-01-08 21:56:40 +0800

    计算属性、监听属性、表单绑定

--- first_vue_demo/first_vue_app/views.py
+++ first_vue_demo/first_vue_app/views.py
@@ -1,4 +1,6 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,26 @@ def demo(request):
 
 def demo2(request):
     return render(request,'vue_demo2.html')
+
+def demo3(request):
+    return render(request,'vue_demo3.html')
+
+def demo4(request):
+    return render(request,'vue_demo4.html')
+
+data = ["apple","orange","banana","lemon","pear"]
+def search(request):
+    if "POST" == request.method:
+        body = json.loads(request.body)
+        print("body:",body)
+        if "key" not in body:
+            return JsonResponse([],safe=False)
+        key = body["key"]
+        ret = []
+        for i in data:
+            if key in i:
+                ret.append(i)
+        return JsonResponse(ret, safe=False)
+
+    else:
+        return HttpResponse(status=404)
